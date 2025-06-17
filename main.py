f5b67e78d9568399dd746025340bae3b56b3bb88
Cy wilkinson <164250786+CythondotNET@users.noreply.github.com>
2025-06-16 22:22:02 -0500

    gun

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ class MyApp(ShowBase):
 
         ShowBase.__init__(self)
 
+        self.gun = self.loader.loadModel('M4A4.glb')
+        self.gun.reparentTo(self.render)
+        self.gun.setScale(1000, 1000, 1000)
+        
+
 app = MyApp()
 app.run()

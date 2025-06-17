aedf9ab04a65fd922ed883f20d822cba72541716
Cy wilkinson <164250786+CythondotNET@users.noreply.github.com>
2025-06-16 22:03:25 -0500

    newstart

--- main.py
+++ main.py
@@ -9,15 +9,5 @@ class MyApp(ShowBase):
 
         ShowBase.__init__(self)
 
-        self.scene = self.loader.loadModel("models/environment")
-
-        self.scene.reparentTo(self.render)
-
-        self.scene.setScale(0.25, 0.25, 0.25)
-        
-        self.scene.setPos(-8, 42, 0)
-
-        
-
 app = MyApp()
 app.run()

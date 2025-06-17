accbf8de1d7ca038e11bb781996a1d3e0e18a3eb
Cy wilkinson <164250786+CythondotNET@users.noreply.github.com>
2025-06-17 15:26:35 -0500

    todo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MyApp(ShowBase):
     def __init__(self):
             
         ShowBase.__init__(self)
-
+                                    #TODO: add a player and controlse
 
         self.gun = self.loader.loadModel('M4A4.glb')
         self.gun.reparentTo(self.render)
@@ -17,15 +17,17 @@ class MyApp(ShowBase):
         self.gun.setPos(-4, 3, 3)
         self.taskMgr.add(self.spinCameraTask, "SpinCamraTask")
         
+
+
     def spinCameraTask(self, task):
 
-        angleDegrees = task.time * 20
+        angleDegrees = task.time * 200
 
         angleRadians = angleDegrees * (pi / 180.0)
 
         self.camera.setPos(20 * sin(angleRadians) - 2, (-20 * cos(angleRadians)) + 10, 3)
 
-        self.camera.setHpr(angleDegrees, 0, 0)
+        self.camera.setHpr(angleDegrees, 0, 90)
 
         return Task.cont
         

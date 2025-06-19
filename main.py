468f47ab02542df38550a4e84f7cc4a4f5a844df
Cy wilkinson <164250786+CythondotNET@users.noreply.github.com>
2025-06-18 20:19:43 -0500

    look

--- main.py
+++ main.py
@@ -5,14 +5,26 @@ from math import pi, sin, cos
 speed = 100.0
 y_delta = 0
 x_delta = 0
+x = 0
+y = 0
+xd = 0
+yd = 0
 
 class MyApp(ShowBase):
 
     def __init__(self):
             
         ShowBase.__init__(self)
+        # Load the environment model.
+
         self.scene = self.loader.loadModel("models/environment")
+
+        # Reparent the model to render.
+
         self.scene.reparentTo(self.render)
+
+        # Apply scale and position transforms on the model.
+
         self.scene.setScale(0.25, 0.25, 0.25)
 
         self.scene.setPos(-8, 42, 0)
@@ -23,13 +35,38 @@ class MyApp(ShowBase):
         self.gun.setScale(500, 500, 500)
         self.gun.setPos(0, 0, 0)
         self.camera.setPos(self.gun.getX() - 0.4, self.gun.getY() - 5, self.gun.getZ() + 8)
-        self.camera.setP(-15)
+        self.camera.setP(self.gun.getP())
         self.gun.setR(-90)
+        self.taskMgr.add(self.look, "look")
+        self.taskMgr.add(self.movche, "check if i moved")
+        
+    def movche(self, task):
         base.buttonThrowers[0].node().setKeystrokeEvent('keystroke')
         self.accept("keystroke", self.move)
+        return Task.cont
+
+    def look(self, task):
+        global xd, yd
+
+
+        if base.mouseWatcherNode.hasMouse():
+            x = base.mouseWatcherNode.getMouseX()
+            y = base.mouseWatcherNode.getMouseY()
+            self.gun.setH(self.gun.getH() + ((xd - x) * 180))
+            self.gun.setP(self.gun.getP() + ((yd - y) * -180))
+            self.camera.setP(self.gun.getP())
+            self.camera.setH(self.gun.getH())
+            xd = x
+            yd = y
+            x = 0 
+            y = 0
+
+        return Task.cont
+    
 
     def move(self, keyname):
         global y_delta, x_delta
+
         if keyname == "w":
             y_delta = (speed * globalClock.get_dt())
         if keyname == "s":
